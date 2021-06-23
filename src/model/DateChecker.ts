3627ab960c219801eebb96c438c846c8c2306d9c
u-kai <76635578+u-kai@users.noreply.github.com>
2021-06-23 22:14:14 +0900

    clear sortAsc but not ref

--- src/model/DateChecker.ts
+++ src/model/DateChecker.ts
@@ -8,4 +8,37 @@ export class DateChecker {
             today.getDate() === compare.getDate()
         )
     }
+    sortAsc = (dates:string[]) => {
+        // let sortList:string[] = [new Date(dates[0]).]
+        let minList:string[] = [dates[0]]
+        // for (let i=0;i<dates.length;i++){
+        //     for (let j=0;j<dates.length;j++){
+        //         if(this.isBiggerDate1(minList[i],dates[i+1])){
+        //             minList[i]
+        //         }
+        //     }
+        // }
+        const keyIsIndexValueIsDateValueTemp:{[key:string]:string} = {}
+        const keyIsDateValueValueIsIndexTemp:{[key:string]:number} = {}
+        const dateToValueList = dates.map((date,i)=>{
+            keyIsDateValueValueIsIndexTemp[this.dateToValue(date).toString()] = i
+            keyIsIndexValueIsDateValueTemp[i.toString()] = date
+            return this.dateToValue(date)
+        })
+        const sortList = dateToValueList.sort()
+        return sortList.map((data)=>{
+            const index = keyIsDateValueValueIsIndexTemp[data]
+            return keyIsIndexValueIsDateValueTemp[index]
+        })
+
+
+
+    }  
+    private dateToValue = (date:string):number => {
+        const dateInfo = new Date(date)
+        return dateInfo.getFullYear() * 12 * 30 + dateInfo.getMonth() * 30 + dateInfo.getDate() + dateInfo.getHours() * 1/24 + dateInfo.getMinutes() * 1/1440
+    } 
+    private isBiggerDate1 = (date1:string,date2:string) => {
+        return this.dateToValue(date1) >= this.dateToValue(date2)
+    }
 }
--- src/tests/model/DataChecker.test.ts
+++ src/tests/model/DataChecker.test.ts
@@ -5,3 +5,11 @@ const dataChecker = new DateChecker()
 it('test is today', () => {
     expect(dataChecker.isToday(today)).toBe(true)
 })
+
+const yestaday = '2021-06-23T01:00:00.000Z'
+const next = "2021-06-23T01:00:00.000Z"
+const list = [today,yestaday,next]
+const compareList = [yestaday,today,next]
+it("test compare",()=>{
+    expect(dataChecker.sortAsc(list)).toStrictEqual(compareList)
+})
